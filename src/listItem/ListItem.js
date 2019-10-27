2e97786418d5534c4492b46f93d10a29fa7d0bdf
pinball24 <pinball101962@gmail.com>
2019-10-27 15:22:18 -0500

    list component

--- src/listItem/ListItem.js
+++ src/listItem/ListItem.js
@@ -9,7 +9,7 @@ export default class ListItem extends Component {
                     <h1>{this.props.volumeInfo.title}</h1>
                     <ul>
                         <li>Author: {this.props.volumeInfo.authors}</li>
-                        <li>Price: ${this.props.saleInfo.textSnippet}</li>
+                        <li>Price: ${this.props.saleInfo.listPrice.amount}</li>
                     </ul>
                 </div>
             </div>
