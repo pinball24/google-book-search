28df56e13c82c6b0f6912b0d37e65a565f84710b
pinball24 <pinball101962@gmail.com>
2019-10-27 16:35:50 -0500

    list item

--- src/listItem/ListItem.js
+++ src/listItem/ListItem.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react';
 
 export default class ListItem extends Component {
     render() {
-        let price = this.props.searchInfo;
+        console.log(this.props.saleInfo)
+        const isForSale = this.props.saleInfo.saleability === "FOR_SALE" || "FOR_SALE_AND_RENTAL"
+            ? <li>Price: ${this.props.saleInfo.amount}</li>
+            : <li>Price: This book is not for sale</li>
+        
         return (
             <div className="ListItem">
                 <div className="books__list">
                     <h1>{this.props.volumeInfo.title}</h1>
                     <ul>
                         <li>Author: {this.props.volumeInfo.authors}</li>
-                        <li>Price: ${this.props.saleInfo.listPrice.amount}</li>
+                        <li>{isForSale}</li>
                     </ul>
                 </div>
             </div>
