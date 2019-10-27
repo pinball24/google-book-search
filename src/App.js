536a477c6867bb9bc7a68ff35f9152fc0f5a1d07
pinball24 <pinball101962@gmail.com>
2019-10-27 14:19:41 -0500

    mapping over books

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
     super(props);
     this.state = {
       searchTerm: '',
-      books: [],
+      books: null,
       filters: {
         printType: 'all',
         bookType: ''
--- src/filterableList/FilterableList.js
+++ src/filterableList/FilterableList.js
@@ -4,20 +4,17 @@ import ListItem from '../listItem/ListItem';
 export default class FilterableList extends Component {
     render() {
         console.log(this.props.books);
-        if (this.props.books && this.props.books.length > 0) {
-        let list = this.props.books.map((book, key) => <ListItem {...book} key={key}/>)
+        if (this.props.books) {
+        let list = this.props.books.items.map((book, key) => <ListItem {...book} key={key}/>);
+        console.log(list)
         } else {
-            return 'There are no books to display'
+            console.log('There are no books to display')
         };
 
         return (
             <div className="filterableList">
-            
+                {list}
             </div>
-        )
+        );
     }
 }
-
-FilterableList.defaultProps = {
-    books: []
-};
